bd49b98e2841dd5462723593aac756d3482f7de9
Yuval Dekel <thedekel@gmail.com>
2013-02-12 18:56:04 -0500

    working, but not finished

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ var posts = [];
 
 app.get("/posts/:id", function(req, res){
   var post_id = parseInt(req.params['id']);
-  console.log(post_id);
   if (post_id >= 0 && post_id < posts.length){
      res.render("view_post", {id: post_id, post: posts[post_id]});
   } else {
@@ -29,13 +28,39 @@ app.post("/posts", function(req, res){
   } else {
     res.send(400);
   }
-  console.log(posts);
 });
 
 app.put("/posts/:id", function(req, res){
+  var post_id = parseInt(req.params['id']);
+  if (post_id && post_id >= 0 && post_id < posts.length){
+     res.render("view_post", {id: post_id, post: posts[post_id]});
+    if (req.body['title'] && req.body['body']){
+      var new_post = {
+        title: req.body['title'],
+        body: req.body['body']
+      };
+      posts[post_id] = new_post;
+      res.redirect(req.url);
+    } else {
+      res.send(400);
+    }
+  } else {
+    res.send(404);
+  }
 });
 
 app.delete("/posts/:id", function(req, res){
+  var post_id = parseInt(req.params['id']);
+  if (post_id && post_id >= 0 && post_id < posts.length){
+    posts.splice(post_id, 1); //splice is a pretty good indie game
+    res.send(200);
+  } else {
+    res.send(404);
+  }
+});
+
+app.get("/", function(req, res){
+   res.render("view_posts", {posts: posts});
 });
 
 app.listen(8080);
